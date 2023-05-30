c64a37ebea3b299c748c39b05ec3a4bdd78eb690
YopterMatbEro <yopter2013@gmail.com>
2023-05-30 13:09:43 +0300

    i'm sure, its finnaly

--- objects.py
+++ objects.py
@@ -55,6 +55,41 @@ class Sale(Base):
     stock = relationship(Stock, backref='sales')
 
 
+class InputHandler:
+    def __init__(self):
+        self.publisher_id = None
+
+    def get_publisher_id(self):
+        while True:
+            publisher = input('Введите имя или id издателя: ')
+            if publisher.isdigit():
+                self.publisher_id = int(publisher)
+                break
+            else:
+                query = session.query(Publisher).filter(Publisher.name.ilike(f'%{publisher}%'))
+                if query.count() == 0:
+                    print(f'Издатель {publisher} не найден в базе данных')
+                elif query.count() == 1:
+                    self.publisher_id = query.first().id
+                    break
+                else:
+                    print(f'Найдено несколько издателей с именем {publisher}. Выберите нужный id из списка:')
+                    publisher_list_id = []
+                    for elem in query.all():
+                        publisher_list_id.append(elem.id)
+                        print(f'{elem.id}: {elem.name}')
+                    while self.publisher_id not in publisher_list_id:
+                        self.publisher_id = input('Введите id издателя: ')
+                        if self.publisher_id == 'exit':
+                            print('Вы закрыли сессию. Всего доброго!')
+                            return
+                        elif not self.publisher_id.isdigit() or int(self.publisher_id) not in publisher_list_id:
+                            print('Некорректный id издателя')
+                        elif int(self.publisher_id) in publisher_list_id:
+                            return int(self.publisher_id)
+        return int(self.publisher_id)
+
+
 def create_tables(engine):
     # Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
